dfd6ee17d1f83000bf98d123f73d57ca631bbb06
Alexandru Marinescu <alexandru.marinescu_19@yahoo.com>
2025-07-18 12:41:18 +0300

    Added first RestController and request.

--- src/main/java/com/alex/Application.java
+++ src/main/java/com/alex/Application.java
@@ -2,12 +2,21 @@ package com.alex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Application {
+@RestController
+public class Application
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
+    @GetMapping
+    public String helloWorld()
+    {
+        return "Hello World Spring Boot!";
+    }
 }
